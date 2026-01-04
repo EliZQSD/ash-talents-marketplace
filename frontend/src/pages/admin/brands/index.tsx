acfa532b27f51396bab28714618de35aa798b399
EliZQSD <elimane077@gmail.com>
2026-01-04 15:27:20 -0500

    Fix TypeScript error in brands page

--- frontend/src/pages/admin/brands/index.tsx
+++ frontend/src/pages/admin/brands/index.tsx
@@ -1,4 +1,4 @@
-index.tsximport { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import AdminNav from '../../../components/AdminNav';
